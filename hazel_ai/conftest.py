fd51a54e0d419ebcc9796af86a64ac30eb2dad4d
affanthegreat <32616893+affanthegreat@users.noreply.github.com>
2023-07-10 13:39:48 +0530

    Initial commit

---
+++ hazel_ai/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from hazel_ai.users.models import User
+from hazel_ai.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ hazel_ai/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from hazel_ai.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
