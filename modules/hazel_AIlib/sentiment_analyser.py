132555fee53dc060ecb9246a6f061ddba0c8ca97
affanthegreat <32616893+affanthegreat@users.noreply.github.com>
2023-07-18 13:16:01 +0530

    added communicator and sentiment anaylsis base class.

---
+++ modules/hazel_AIlib/sentiment_analyser.py
@@ -0,0 +1,15 @@
+
+
+
+class HazelSentimentAnalyser():
+
+    def __init__(self):
+        pass
+
+    def generate_sentiment_value(self,text):
+        pass
+
+    def generate_emotion_state(self, text):
+        pass
+
+
--- modules/hazelcommunicator/communicator.py
+++ modules/hazelcommunicator/communicator.py
@@ -1,37 +1,72 @@
 import logging
-
+import requests
 
 
 class HazelComunicator():
-    def __init__(self):
+    def __init__(self,hazel_core_server):
         logging.info("Hazel Communicator Object has been created.")
 
-    def get_user_followers(self):
-        pass
+        self.HAZEL_SERVER = hazel_core_server
+        self.API_SUB_URL = self.HAZEL_SERVER + 'communicator/'
+        create_url = lambda path: self.API_SUB_URL + path
 
-    def get_user_following(self):
-        pass
 
-    def get_user_metrics(self):
-        pass
+        self.STREAM_USERS_BY_TOPIC_ENDPOINT = create_url('stream_users_by_topic')
+        self.UPDATE_USER_TOPICS_ENDPOINT = create_url('update_user_topics')
 
-    def update_user_metrics(self):
-        pass
+        # Leaf Side Endpoints
+        self.STREAM_UNCATEGORIZED_LEAVES_ENDPOINT = create_url('stream_uncategorized_leaves')
+        self.STREAM_LEAVES_TOPIC_WISE_ENDPOINT = create_url('stream_leaves_topic_wise')
+        self.STREAM_NEGATIVE_LEAVES_ENDPOINT = create_url('stream_negative_leaves')
+        self.STREAM_UNMARKED_COMMENTS_ENDPOINT = create_url('stream_unmarked_comments')
+        self.STREAM_MARKED_COMMENTS_ENDPOINT = create_url('stream_marked_comments')
+        self.SEND_LEAF_METRICS_ENDPOINT = create_url('send_leaf_metrics')
+        self.UPDATE_BATCH_LEAF_METRICS = create_url('update_batch_leaf_metrics')
 
-    def get_users_by_topic(self):
-        pass
+    def make_post_request(self,api_endpoint,body):
+        return requests.post(api_endpoint,json=body)
 
-    def get_leaves_by_topic(self, topic):
-        pass
+    def stream_users_by_topic(self, topic_id,page_number=1):
+        request_body = {'page_number': page_number, 'topic_id': topic_id}
+        response= self.make_post_request(self.STREAM_USERS_BY_TOPIC_ENDPOINT,  request_body)
+        return response
 
-    def get_comments_by_topic(self):
-        pass
+    def update_user_metrics(self, topic_id, user_id):
+        request_body = {'user_id': user_id, 'topic_id': topic_id}
+        response = self.make_post_request(self.UPDATE_USER_TOPICS_ENDPOINT, request_body)
+        return response
 
-    def get_user_leaves(self):
-        pass
+    def get_leaf_metrics(self, leaf_id):
+        request_body = {'leaf_id': leaf_id}
+        response = self.make_post_request(self.SEND_LEAF_METRICS_ENDPOINT, request_body)
+        return response
 
-    def get_leaf_metrics(self):
-        pass
+    def stream_uncategorized_leaves(self,page_number=1):
+        request_body = {'page_number': page_number}
+        response = self.make_post_request(self.STREAM_UNCATEGORIZED_LEAVES_ENDPOINT,request_body)
+        return response
 
-    def update_leaf_metrics(self):
-        pass
+    def stream_leaves_topic_wise(self,topic_id, page_number= 1):
+        request_body = {'page_number': page_number, 'topic_id': topic_id}
+        response = self.make_post_request(self.STREAM_LEAVES_TOPIC_WISE_ENDPOINT, request_body)
+        return response
+
+    def stream_negative_leaves(self,page_number=1):
+        request_body = {'page_number': page_number}
+        response = self.make_post_request(self.STREAM_NEGATIVE_LEAVES_ENDPOINT,request_body)
+        return response
+
+    def stream_unmarked_comments(self,leaf_id, page_number=1):
+        request_body = {'page_number': page_number, 'leaf_id':leaf_id}
+        response = self.make_post_request(self.STREAM_UNMARKED_COMMENTS_ENDPOINT,request_body)
+        return response
+
+    def stream_marked_comments(self,leaf_id, page_number=1):
+        request_body = {'page_number': page_number, 'leaf_id':leaf_id}
+        response = self.make_post_request(self.STREAM_MARKED_COMMENTS_ENDPOINT,request_body)
+        return response
+
+    def update_batch_leaf_metrics(self,batch_number,leaves_collection):
+        request_body = {'batch_number':batch_number, 'leaves_collection': leaves_collection}
+        response = self.make_post_request(self.UPDATE_BATCH_LEAF_METRICS, request_body)
+        return response
