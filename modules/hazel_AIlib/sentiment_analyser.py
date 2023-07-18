7fee3524f11a88fafa682b13e6a4fb9a3e409d07
affanthegreat <32616893+affanthegreat@users.noreply.github.com>
2023-07-18 13:38:32 +0530

    added sentiment analyser

--- modules/hazel_AIlib/sentiment_analyser.py
+++ modules/hazel_AIlib/sentiment_analyser.py
@@ -1,15 +1,32 @@
-
+import logging
+from transformers import pipeline
 
 
 class HazelSentimentAnalyser():
 
     def __init__(self):
-        pass
+        self.sentiment_pipeline = pipeline("sentiment-analysis")
+        self.emotion_pipeline = pipeline("text-classification", model='bhadresh-savani/distilbert-base-uncased-emotion')
+        logging.info("Hazel Sentiment Analyser v1")
 
     def generate_sentiment_value(self,text):
-        pass
+        self.sentiment_pipeline([text])
 
     def generate_emotion_state(self, text):
-        pass
+        self.emotion_pipeline(text)
 
 
+    def process(self,text):
+        sentiment_value = self.generate_sentiment_value(text)[0]
+        emotional_state = self.generate_emotion_state(text)[0]
+        if sentiment_value['label'] == "NEGATIVE":
+            sentiment_percentage = -1 * sentiment_value['score']
+        else:
+            sentiment_percentage = sentiment_value['score']
+
+        sentiment_dict = {
+            'sentiment_value': sentiment_percentage,
+            'emotional_state': emotional_state['label']
+        }
+
+        return sentiment_dict
